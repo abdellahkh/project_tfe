3b419e76a4a6c6b5e0152ace60d4a590c7ce319d
abdellahkh <abdellah.khleeh@gmail.com>
2023-11-12 20:51:08 +0100

    update modele voiture

--- car_dealer/models.py
+++ car_dealer/models.py
@@ -13,16 +13,64 @@ class Modele(models.Model):
     def __str__(self):
         return f"{self.nom}"
 
-
+       
 class Voiture(models.Model):
+    CARBURANT_CHOICES = [
+        ('Diesel', 'Diesel'),
+        ('Essence', 'Essence'),
+        ('Ethanol', 'Ethanol'),
+        ('Electrique', 'Electrique'),
+        ('Hydrogene', 'Hydrogene'),
+        ('LPG', 'LPG'),
+        ('CNG', 'CNG'),
+        ('Hybride (Elec - Ess)', 'Hybride (Elec - Ess)'),
+        ('Hybride (Elec - Diesel)', 'Hybride (Elec - Diesel)'),
+        ('Autres', 'Autres'),
+    ]
+    
+
     marque = models.ForeignKey(Marque, on_delete=models.PROTECT, related_name='voitures', help_text="Marque de la voiture")
     modele = models.ForeignKey(Modele, on_delete=models.CASCADE, help_text="Modèle de la voiture")
     annee_fabrication = models.PositiveIntegerField(help_text="Année de fabrication")
+    carburant = carburant = models.CharField(
+        null = True,
+        blank = True,
+        max_length=90,
+        choices=CARBURANT_CHOICES,
+        help_text="Type de carburant de la voiture"
+    )
     kilometrage = models.PositiveIntegerField(help_text="Kilométrage actuel")
-    prix = models.DecimalField(max_digits=10, decimal_places=2, help_text="Prix de la voiture")
+    cruise_control = models.BooleanField(default=False, verbose_name="Cruise Control")
+    direction_assistee = models.BooleanField(default=False, verbose_name="Direction assistée")
+    audio_interface = models.BooleanField(default=False, verbose_name="Audio Interface")
+    airbags = models.BooleanField(default=False, verbose_name="Airbags")
+    air_conditionne = models.BooleanField(default=False, verbose_name="Air Conditioninne")
+    siege_chauffant = models.BooleanField(default=False, verbose_name="Siege Chauffant")
+    alarm_system = models.BooleanField(default=False, verbose_name="Alarm System")
+    parkassist = models.BooleanField(default=False, verbose_name="Park Assist")
+    camera_recul = models.BooleanField(default=False, verbose_name="Camera de Recul")
+    start_stop = models.BooleanField(default=False, verbose_name="Start Stop")
+    essui_auto = models.BooleanField(default=False, verbose_name="Essui-glace Auto")
+    car_play = models.BooleanField(default=False, verbose_name="Car Play-System")
+
+    
     description = models.TextField(blank=True, null=True, help_text="Description de la voiture")
     date_poste = models.DateTimeField(auto_now_add=True, help_text="Date de publication de l'annonce")
-    photos = models.ImageField(upload_to='images/voitures/', blank=True, null=True, help_text="Image de la voiture")
+    #photos_1 = models.ImageField(upload_to='images/voitures/', blank=True, null=True, help_text="Image de la voiture")
+
+
+    
+    car_photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_7 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_8 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_9 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+
+    prix = models.DecimalField(max_digits=10, decimal_places=2, help_text="Prix de la voiture")
 
     class Meta:
         ordering = ['-date_poste']  # Tri par date de publication par défaut
@@ -31,8 +79,3 @@ class Voiture(models.Model):
         return f"({self.marque}) {self.modele} - {self.annee_fabrication}"
 
 
-class Photo(models.Model):
-    image = models.ImageField(upload_to='images/voitures/', help_text="Image de la voiture")
-
-    def __str__(self):
-        return f"Photo {self.id}"

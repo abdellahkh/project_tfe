cfd19212d5ab2162d8a1f3282a3974c90b4ba40b
abdellahkh <abdellah.khleeh@gmail.com>
2023-11-18 22:43:11 +0100

    creation de template, ajout des pages login, register, about, allServices

--- car_dealer/views.py
+++ car_dealer/views.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Voiture
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     voitures = Voiture.objects.all()
-
     return render(request, 'home.html', {'voitures' :voitures})
+
+def register_user(request):
+    return render(request, 'register.html', {})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("Vous etes connecte..."))
+            return redirect('home')
+        else:
+            messages.success(request, ("Une erreur est survenue, veuillez reessayer..."))
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("Vous avez ete deconnecte..."))
+    return redirect('home')
+
+
+
+def about(request):
+    return render(request, 'about.html', {})
+def allServices(request):
+    return render(request, 'allServices.html', {})

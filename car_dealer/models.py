1013859aa5c9dcdef4c00afa817938665ec2a7d2
abdellahkh <abdellah.khleeh@gmail.com>
2023-11-28 19:00:38 +0100

    save, looks good

--- car_dealer/models.py
+++ car_dealer/models.py
@@ -12,7 +12,7 @@ class Member(AbstractUser):
     username = models.CharField(max_length=100, unique=True, help_text="username")
     first_name = models.CharField(max_length=100, help_text="Prenom")
     last_name = models.CharField(max_length=100, help_text="Nom")
-    email = models.EmailField(max_length=100, help_text="Nom du modèle")
+    email = models.EmailField(max_length=100, help_text="Email")
     phone = models.CharField(max_length=100, help_text="Phone", blank=True, null=True)
     address = models.CharField(max_length=100, help_text="adresse", blank=True, null=True)
     postal = models.IntegerField(null=True, blank=True, help_text="Code Postal")
@@ -205,6 +205,37 @@ class Review(models.Model):
     def __str__(self):
         return f"({self.user_id.username}) : {self.comment}"
 
+class Demande(models.Model):
+    GENRE_INTERVENTION = [
+        ('Entretien', 'Entretien'),
+        ('Reparation, panne', 'Reparation, panne'),
+        ('Carrosserie', 'Carrosserie')
+    ]
+    member = models.ForeignKey(Member, on_delete=models.CASCADE,blank=True, null=True, help_text="Service selectionner")
+    first_name = models.CharField(max_length=100,blank=True, null=True, help_text="Prenom")
+    last_name = models.CharField(max_length=100, blank=True, null=True, help_text="Nom")
+    email = models.EmailField(max_length=100,blank=True, null=True, help_text="Email")
+    phone = models.CharField(max_length=100, help_text="Phone", blank=True, null=True)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, help_text="Service selectionner")
+    date = models.DateTimeField(auto_now_add=True, help_text="Date de la demande")
+    date_desiree = models.DateTimeField(auto_now_add=False, blank=True, null=True, help_text="  (Date d'intervention souhaitée)")
+    details = models.TextField(blank=True, null=True, help_text="Details specifique")
+    genre = models.CharField(
+        null=True,
+        blank=True,
+        max_length=90,
+        choices=GENRE_INTERVENTION,
+        help_text="Type de service"
+    )
+    startLocation = models.CharField(max_length=100, help_text="Commune depart", blank=True, null=True)
+    endLocation = models.CharField(max_length=100, help_text="Commune arrivee", blank=True, null=True)
+    car_doc = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return f"{self.service} : {self.date_desiree}"
+
+
+
         
 
 class Vente(models.Model):
@@ -221,36 +252,14 @@ class Vente(models.Model):
     )
     paid = models.BooleanField(default=False, verbose_name="Payé")
     user_id = models.ForeignKey(Member, on_delete=models.CASCADE, help_text="Identifiant du User")
-    service_id = models.ForeignKey(Service, blank=True, null=True, on_delete=models.CASCADE, help_text="Identifiant du Service")
+    demande_id = models.ForeignKey(Demande, blank=True, null=True, on_delete=models.CASCADE, help_text="Identifiant du Service")
     voiture_id = models.ForeignKey(Voiture, blank=True, null=True,  on_delete=models.CASCADE, help_text="Identifiant de la voiture")
     date = models.DateTimeField(auto_now_add=True, help_text="Date de la vente")
     montant_total = models.DecimalField(max_digits=10, decimal_places=0, blank=True, null=True, help_text="Montant total de la vente")
     montant_acompte = models.DecimalField(max_digits=10, decimal_places=0, blank=True, null=True, help_text="Montant de l'acompte")
 
 
-class Demande(models.Model):
-    GENRE_INTERVENTION = [
-        ('Entretien', 'Entretien'),
-        ('Reparation, panne', 'Reparation, panne'),
-        ('Carrosserie', 'Carrosserie')
-    ]
 
-    service = models.ForeignKey(Service, on_delete=models.CASCADE, help_text="Service selectionner")
-    date = models.DateTimeField(auto_now_add=True, help_text="Date de la demande")
-    date_desiree = models.DateTimeField(auto_now_add=False, blank=True, null=True, help_text="  (Date d'intervention souhaitée)")
-    details = models.TextField(blank=True, null=True, help_text="Details specifique")
-    genre = models.CharField(
-        null=True,
-        blank=True,
-        max_length=90,
-        choices=GENRE_INTERVENTION,
-        help_text="Type de service"
-    )
-    startLocation = models.CharField(max_length=100, help_text="Commune depart", blank=True, null=True)
-    endLocation = models.CharField(max_length=100, help_text="Commune arrivee", blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.service} : {self.date_desiree}"
 
     
 

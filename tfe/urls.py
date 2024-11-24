fecef9c0f097fa9f4a6377d3f062b599373aaa48
abdellahkh <abdellah.khleeh@gmail.com>
2024-11-24 10:42:31 +0100

    res

--- tfe/urls.py
+++ tfe/urls.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from . import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+] 
+
+urlpatterns += i18n_patterns(
     path('api/', include('api.urls')),
     path('', include('car_dealer.urls')),
-    path('cart/', include('cart.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('cart/', include('cart.urls')),    
+)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
